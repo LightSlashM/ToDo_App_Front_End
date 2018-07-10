4b4daefbec520d0dd4d91ec04313996d81f337fa
Maxym Wolberg <wolberg.maxym91@bloomfield.org>
2018-07-10 18:37:59 +0000

    added a strikethrough when checkbox is checked as well as an appearing x

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ $(document).ready(function({
 )
 */
 $(document).ready(function(){
+    $(".material-icons").hide()
     console.log("Im ready")
     $("#add-todo-btn").click(function(){
         console.log("Buttons are now functional")
@@ -29,5 +30,13 @@ $(document).ready(function(){
         console.log(timeOfTodo)
 
         $("ul").append("<li> <input type='checkbox' class='check'><a href='show.html' style='text-decoration:none'> "+nameOfTodo+" </a><span class='day-est'>"+timeOfTodo+" day(s)</span></li>")
-    })
+            })
+            $("ul").on("click","input[type='checkbox']",function(){
+        $(this).closest("li").toggleClass("completed-todo")
+        
+            })
+            $("li").hover(function(){$(".material-icons").show();},
+                   function(){$(".material-icons").hide();});
+
+                    
 })
